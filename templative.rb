033baa6356a1d2551e72a36228c7cf6dc4d983ce
Oliver Barnum <oliverbarnum32@gmail.com>
2020-03-31 19:51:18 -0700

    Do the thing

--- templative.rb
+++ templative.rb
@@ -1,4 +1,4 @@
-class Templative < Formula 
+class Templative < Formula
   include Language::Python::Virtualenv
 
   desc "Automate card sheet -> TCG"
@@ -32,6 +32,7 @@ class Templative < Formula
   depends_on "openssl@1.1"
   depends_on "pcre"
   depends_on "pixman"
+  depend_on "librsvg"
   depends_on "python"
   depends_on "readline"
   depends_on "shared-mime-info"
@@ -174,8 +175,13 @@ class Templative < Formula
   end
 
   resource "Pillow" do
-    url "https://files.pythonhosted.org/packages/39/47/f28067b187dd664d205f75b07dcc6e0e95703e134008a14814827eebcaab/Pillow-7.0.0.tar.gz"
-    sha256 "4d9ed9a64095e031435af120d3c910148067087541131e82b3e8db302f4c8946"
+    url "https://github.com/GlezSeoane/Pillow/archive/6.1.0.tar.gz"
+    sha256 "9a3a613e7780362605b10765274389b3e9a6fe3201dd2bceeb422d45c5c9ba18"
+  end
+  
+  resource "filetype" do
+    url "https://files.pythonhosted.org/packages/e8/53/298887541ae479f8467d4d23e028c6d15f9811da25c582297fd3869666b7/filetype-1.0.5.tar.gz"
+    sha256 "17a3b885f19034da29640b083d767e0f13c2dcb5dcc267945c8b6e5a5a9013c7"
   end
 
   resource "pycparser" do
@@ -300,6 +306,25 @@ class Templative < Formula
 
   def install
     virtualenv_install_with_resources(:using => "python@3.7")
+
+    resource("Pillow").stage do
+      inreplace "setup.py" do |s|
+        sdkprefix = MacOS.sdk_path_if_needed ? MacOS.sdk_path : ""
+        s.gsub! "openjpeg.h", "probably_not_a_header_called_this_eh.h"
+        s.gsub! "ZLIB_ROOT = None", "ZLIB_ROOT = ('#{sdkprefix}/usr/lib', '#{sdkprefix}/usr/include')"
+        s.gsub! "JPEG_ROOT = None", "JPEG_ROOT = ('#{Formula["jpeg"].opt_prefix}/lib', '#{Formula["jpeg"].opt_prefix}/include')"
+        s.gsub! "FREETYPE_ROOT = None", "FREETYPE_ROOT = ('#{Formula["freetype"].opt_prefix}/lib', '#{Formula["freetype"].opt_prefix}/include')"
+      end
+
+      ENV.append "CFLAGS", "-I#{MacOS.sdk_path}/System/Library/Frameworks/Tk.framework/Versions/8.5/Headers" unless MacOS::CLT.installed?
+      venv.pip_install Pathname.pwd
+    end
+    res = resources.map(&:name).to_set - ["Pillow"]
+    res.each do |r|
+      venv.pip_install resource(r)
+    end
+
+    venv.pip_install_and_link buildpath
   end
 
   test do
